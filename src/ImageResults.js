7208042f03df827a3decd607cf016b69d70e41a1
Kayleonia <julie_nardino@hotmail.com>
2023-07-24 19:06:33 -0700

    Styled the images display

--- src/ImageResults.js
+++ src/ImageResults.js
@@ -3,13 +3,14 @@ import React from 'react';
 export default function ImageResults(props) {
     if (props.photos) {
     return (
-        <section className="results result-list">
+        <section className="results result-images">
             <div className="row">
                 {props.photos.map(function(photos, index) {
                     return (
                         <div className="col-4" key={index}>
-                            <img src={photos.src.landscape} alt={photos.alt} className="img-fluid" />
-                        </div>)
+                            <img src={photos.src.landscape} alt={photos.alt} className="img-fluid images" />
+                        </div>
+                        )
                 })}
             </div>
         </section>)
